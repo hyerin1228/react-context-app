e993a6e1204bcc11b7d425f2ccb43e9dd3154ce2
hyerin1228 <hyerin1228@gmail.com>
2024-02-07 14:00:09 +0900

    #4. 주문 완료 페이지 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import SummaryPage from './pages/SummaryPage';
 import OrderPage from './pages/OrderPage';
 import CompletePage from './pages/CompletePage';
+import { useState } from 'react';
 
 function App() {
+  const [step, setStep] = useState(0)
   return (
-    <div style={{padding: '4rem' }}>
-      <OrderPage />
-      <SummaryPage />
-      <CompletePage />
+    <div style={{pafirst: '4rem' }}>
+      {step === 0 && <OrderPage setStep={setStep} /> } 
+      {step === 1 && <SummaryPage setStep={setStep} /> }
+      {step === 2 && <CompletePage setStep={setStep} /> }
     </div>
   );
 }
